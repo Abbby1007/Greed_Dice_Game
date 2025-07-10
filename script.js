39a85b9d801fd45ffb6e63db6fe2a7e442b4e99c
Abbby1007 <Abigailloken25@gmail.com>
2025-07-10 08:44:21 -0500

    update for starting Number

--- script.js
+++ script.js
@@ -37,24 +37,27 @@ document.getElementById("firstBut").innerHTML = `<a href="kRound.html"> <button>
 
 
 function start(){
-   
+
+document.getElementById("startNum2").innerHTML = `<h3> Starting Number: `;
+
+
 document.getElementById("rules").innerHTML = `<h3> You will have a total of 3 Rounds to get the highest Score possible </h3>
 <h3> If you roll a total of more then 20 you loose all your points and oly keep the starting number </h3> 
 <h3> You can choose to not move on to the next round and keep your score</h3>`
-const oneDiceOne = Math.floor(Math.random() * 6) + 1;
-const oneDiceTwo = Math.floor(Math.random() * 6) + 1;
-const oneDiceThree = Math.floor(Math.random() * 6) + 1;
-const oneDiceFour = Math.floor(Math.random() * 6) + 1;
-const oneDiceFive = Math.floor(Math.random() * 6) + 1;
-const oneDiceSix = Math.floor(Math.random() * 6) + 1;
+const diceOne = Math.floor(Math.random() * 6) + 1;
+const diceTwo = Math.floor(Math.random() * 6) + 1;
+const diceThree = Math.floor(Math.random() * 6) + 1;
+const diceFour = Math.floor(Math.random() * 6) + 1;
+const diceFive = Math.floor(Math.random() * 6) + 1;
+const diceSix = Math.floor(Math.random() * 6) + 1;
 
 diceDisplay1.textContent = ` Dice 1: ${diceOne}    Dice 2: ${diceTwo}  Dice 3: ${diceThree}`
 
 diceDisplay2.textContent = ` Dice 4: ${diceFour}    Dice 5: ${diceFive}  Dice 6: ${diceSix}`
 
-const totalScore = StartingNum  + oneDiceOne + oneDiceTwo + oneDiceThree + oneDiceFour + oneDiceFive + oneDiceSix;
+// const totalScore = StartingNum  + oneDiceOne + oneDiceTwo + oneDiceThree + oneDiceFour + oneDiceFive + oneDiceSix;
 const totalRoll = oneDiceOne + oneDiceTwo + oneDiceThree + oneDiceFour + oneDiceFive + oneDiceSix;
 
 totalDiceDisplay.textContent  =` Total Dice Roll: ${totalRoll}`
-totalDisplay.textContent = `Total Score: ${totalScore}`
+// totalDisplay.textContent = `Total Score: ${totalScore}`
 }
